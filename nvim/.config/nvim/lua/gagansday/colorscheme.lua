ec4898ef608f8df2c0e88ced1b879bf0b8b88faf
Gagandeep Singh <gagansday@gmail.com>
2022-06-05 08:53:02 +0530

    update colorscheme

--- nvim/.config/nvim/lua/gagansday/colorscheme.lua
+++ nvim/.config/nvim/lua/gagansday/colorscheme.lua
@@ -1,8 +1,12 @@
-vim.cmd [[
-try
-  colorscheme onedark
-catch /^Vim\%((\a\+)\)\=:E185/
-  colorscheme default
-  set background=dark
-endtry
-]]
+-- vim.cmd [[
+-- try
+--  colorscheme onedark
+-- catch /^Vim\%((\a\+)\)\=:E185/
+--  colorscheme default
+--  set background=dark
+-- endtry
+-- ]]
+
+vim.o.background = "dark" -- to load onedark
+-- vim.o.background = "light" -- to load onelight
+require("onedarkpro").load()
--- nvim/.config/nvim/lua/gagansday/plugins.lua
+++ nvim/.config/nvim/lua/gagansday/plugins.lua
@@ -61,7 +61,8 @@ return packer.startup(function(use)
 
   -- Colorschemes
   -- use "lunarvim/colorschemes" -- A bunch of colorschemes you can try out
-  use "joshdick/onedark.vim"
+  -- use "joshdick/onedark.vim"
+  use 'olimorris/onedarkpro.nvim'
 
   -- cmp plugins
   use "hrsh7th/nvim-cmp" -- The completion plugin
