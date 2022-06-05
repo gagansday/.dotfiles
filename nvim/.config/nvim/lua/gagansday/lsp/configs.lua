1922a7c5d25632ad0209d0f83425fadca9e24c74
Gagandeep Singh <gagansday@gmail.com>
2022-06-05 15:35:03 +0530

    update lsp config

--- nvim/.config/nvim/lua/gagansday/lsp/configs.lua
+++ nvim/.config/nvim/lua/gagansday/lsp/configs.lua
@@ -1,6 +1,6 @@
 local status_ok, lsp_installer = pcall(require, "nvim-lsp-installer")
 if not status_ok then
-	return
+  return
 end
 
 local lspconfig = require("lspconfig")
@@ -8,17 +8,17 @@ local lspconfig = require("lspconfig")
 local servers = { "jsonls", "sumneko_lua", "intelephense" }
 
 lsp_installer.setup {
-	ensure_installed = servers
+  ensure_installed = servers
 }
 
 for _, server in pairs(servers) do
-	local opts = {
-		on_attach = require("gagansday.lsp.handlers").on_attach,
-		capabilities = require("gagansday.lsp.handlers").capabilities,
-	}
-	local has_custom_opts, server_custom_opts = pcall(require, "gagansday.lsp.settings." .. server)
-	if has_custom_opts then
-	 	opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
-	end
-	lspconfig[server].setup(opts)
+  local opts = {
+    on_attach = require("gagansday.lsp.handlers").on_attach,
+    capabilities = require("gagansday.lsp.handlers").capabilities,
+  }
+  local has_custom_opts, server_custom_opts = pcall(require, "gagansday.lsp.settings." .. server)
+  if has_custom_opts then
+    opts = vim.tbl_deep_extend("force", server_custom_opts, opts)
+  end
+  lspconfig[server].setup(opts)
 end
