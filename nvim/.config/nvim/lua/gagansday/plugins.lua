1a10850563273962fc48f5fc5040a393f34395f1
Gagandeep Singh <gagansday@gmail.com>
2022-06-06 07:28:50 +0530

    update lsp handlers

--- nvim/.config/nvim/lua/gagansday/plugins.lua
+++ nvim/.config/nvim/lua/gagansday/plugins.lua
@@ -115,9 +115,6 @@ return packer.startup(function(use)
   -- Laravel
   use "jwalton512/vim-blade"
 
-  -- TailwindCSS
-  use "steelsojka/headwind.nvim"
-
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
