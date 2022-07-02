b58e3ec90b0ce2676ae7407a3373d5d1d2229a54
Gagandeep Singh <gagansday@gmail.com>
2022-07-02 20:59:05 +0530

    autopairs

--- nvim/.config/nvim/lua/gagansday/plugins.lua
+++ nvim/.config/nvim/lua/gagansday/plugins.lua
@@ -70,6 +70,7 @@ return packer.startup(function(use)
 
 	use("lewis6991/gitsigns.nvim")
 	use("lukas-reineke/indent-blankline.nvim")
+	use("windwp/nvim-autopairs")
 
 	-- Automatically set up your configuration after cloning packer.nvim
 	-- Put this at the end after all plugins
