5e57efc756257ef84cf46aa82f942f165c606f59
Gagandeep Singh <gagansday@gmail.com>
2022-07-06 13:08:19 +0530

    fix: remove todo

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -14,12 +14,3 @@ require("gagansday.nvim-tree")
 require("gagansday.lualine")
 require("gagansday.keymaps")
 require("gagansday.autocommands")
-
--- Tasks
--- 1. LSP (native) -> done
--- 2. Formater (null-ls) -> done
--- 3. CMP -> done
--- 4. telescope -> done
--- 5. comment -> done
--- 6. keymap
--- 7. autocommands
