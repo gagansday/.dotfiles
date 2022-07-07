936947876807d9786dca577142792709c8ca5cd9
Gagandeep Singh <gagansday@gmail.com>
2022-07-07 08:36:34 +0530

    fix: remove old neovim config

--- nvim/.config/nvim.old/lua/gagansday/lsp/init.lua
+++
@@ -1,8 +0,0 @@
-local status_ok, _ = pcall(require, "lspconfig")
-if not status_ok then
-  return
-end
-
-require "gagansday.lsp.configs"
-require("gagansday.lsp.handlers").setup()
--- require("gagansday.lsp.null-ls")
