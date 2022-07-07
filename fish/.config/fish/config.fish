1e4f873211a76d6914e66798faf379e0b0707452
Gagandeep Singh <gagansday@gmail.com>
2022-07-07 08:26:19 +0530

    move fish config to dotfiles

---
+++ fish/.config/fish/config.fish
@@ -0,0 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+alias vim=nvim
+alias air-backend=~/scripts/air-backend.sh
+alias air-frontend=~/scripts/air-frontend.sh
+alias dotfiles=~/scripts/dotfiles.sh
