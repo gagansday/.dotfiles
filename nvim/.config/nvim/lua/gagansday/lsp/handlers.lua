6cc06a035bd45a61916c909a2bdab77877ea7af4
Gagandeep Singh <gagansday@gmail.com>
2022-12-27 17:33:23 +0530

    feat: update lsp handler

--- nvim/.config/nvim/lua/gagansday/lsp/handlers.lua
+++ nvim/.config/nvim/lua/gagansday/lsp/handlers.lua
@@ -63,14 +63,11 @@ M.on_attach = function(client, bufnr)
   })
 end
 
-local capabilities = vim.lsp.protocol.make_client_capabilities()
-
 local status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not status_ok then
   return
 end
 
--- M.capabilities = cmp_nvim_lsp.update_capabilities(capabilities)
-M.capabilities = require("cmp_nvim_lsp").default_capabilities()
+M.capabilities = cmp_nvim_lsp.default_capabilities()
 
 return M
