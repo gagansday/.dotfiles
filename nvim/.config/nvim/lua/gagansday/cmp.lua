e377f77771f69c9ac42a479eefb9988bb139d75b
Gagandeep Singh <gagansday@gmail.com>
2023-01-07 12:13:18 +0530

    feat: add icons in nvm cmp

--- nvim/.config/nvim/lua/gagansday/cmp.lua
+++ nvim/.config/nvim/lua/gagansday/cmp.lua
@@ -60,7 +60,10 @@ cmp.setup {
 
   formatting = {
     format = lspkind.cmp_format {
+      mode = "symbol", -- show only symbol annotations
       with_text = true,
+      maxwidth = 50, -- prevent the popup from showing more than provided characters (e.g 50 will not show more than 50 characters)
+      ellipsis_char = "...", -- when popup menu exceed maxwidth, the truncated part would show ellipsis_char instead (must define maxwidth first)
       menu = {
         buffer = "[buf]",
         nvim_lsp = "[lsp]",
